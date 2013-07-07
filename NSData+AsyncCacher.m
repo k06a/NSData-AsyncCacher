e1b6e493e87f983e57d6d9cbe90efa507921013f
Anton Bukov <k06aaa@gmail.com>
2013-07-07 23:23:00 +0400

    Using different queues and NSCache also

--- NSData+AsyncCacher.m
+++ NSData+AsyncCacher.m
@@ -12,19 +12,32 @@
 
 + (void)getDataFromURL:(NSURL *)url toBlock:(void(^)(NSData * data, BOOL * retry))block
 {
-    static NSOperationQueue * queue;
+    static NSCache * cache;
+    static NSOperationQueue * mainQueue;
+    static NSOperationQueue * parallelQueue;
     static NSMutableDictionary * blocksDict;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        queue = [[NSOperationQueue alloc] init];
-        queue.maxConcurrentOperationCount = 64;
+        cache = [[NSCache alloc] init];
+        mainQueue = [[NSOperationQueue alloc] init];
+        mainQueue.maxConcurrentOperationCount = 1;
+        parallelQueue = [[NSOperationQueue alloc] init];
+        parallelQueue.maxConcurrentOperationCount = 64;
         blocksDict = [NSMutableDictionary dictionary];
         
         NSURLCache *URLCache = [[NSURLCache alloc] initWithMemoryCapacity:4*1024*1024 diskCapacity:20*1024*1024 diskPath:nil];
         [NSURLCache setSharedURLCache:URLCache];
     });
     
-    dispatch_async(dispatch_get_main_queue(), ^{
+    [mainQueue addOperationWithBlock:^{
+        if ([cache objectForKey:url])
+        {
+            BOOL retry = NO;
+            block([cache objectForKey:url], &retry);
+            if (!retry)
+                return;
+        }
+        
         if (blocksDict[url] == nil)
             blocksDict[url] = [NSMutableArray array];
         
@@ -33,25 +46,33 @@
         if (blocks.count != 1)
             return;
         
-        [queue addOperationWithBlock:^
+        [parallelQueue addOperationWithBlock:^
         {
-            [NSURLConnection sendAsynchronousRequest:[NSURLRequest requestWithURL:url] queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse * response, NSData * data, NSError * error)
-            {
+            NSError * error;
+            NSURLResponse * response;
+            NSData * data = [NSURLConnection sendSynchronousRequest:[NSURLRequest requestWithURL:url] returningResponse:&response error:&error];
+            
+            [mainQueue addOperationWithBlock:^{
+                if (data)
+                    [cache setObject:data forKey:url];
+                
                 for (id a in blocks)
                 {
                     void(^aBlock)(NSData *,BOOL *) = a;
                     if ((id)aBlock == [NSNull null])
                         continue;
                     
-                    BOOL retry = NO;
-                    aBlock(data, &retry);
-                    if (retry)
-                        [NSData getDataFromURL:url toBlock:aBlock];
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        BOOL retry = NO;
+                        aBlock(data, &retry);
+                        if (retry)
+                            [NSData getDataFromURL:url toBlock:aBlock];
+                    });
                 }
                 [blocks removeAllObjects];
             }];
         }];
-    });
+    }];
 }
 
 @end
